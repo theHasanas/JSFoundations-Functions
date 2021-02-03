f169c1b2623450dd9a6b20561e8e6a955a296cd0
theHasanas <30086691+theHasanas@users.noreply.github.com>
2021-02-03 17:01:54 +0300

    Solved task!

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,7 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  return n % 2 === 1;
 }
 
 /**
@@ -33,7 +33,11 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  if (isOdd(n)) {
+    return (n - 1) / 2;
+  }
+
+  return n / 2;
 }
 
 /**
@@ -47,7 +51,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (isOdd(n)) {
+    return n * n;
+  }
+
+  return 2 * n;
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
